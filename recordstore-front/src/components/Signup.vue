52ff65c28ced858e48a4c156b599fcbdbf4d4eef
smdeox <smdeox@live.com>
2020-08-25 10:20:47 -0400

    Debugging post routes

--- recordstore-front/src/components/Signup.vue
+++ recordstore-front/src/components/Signup.vue
@@ -1,73 +1,82 @@
 <template>
-  <div class="max-w-sm m-auto my-8">
-    <div class="border p-10 border-grey-light shadow rounded">
-      <h3 class="text-2xl mb-6 text-grey-darkest">Sign Up</h3>
-      <form @submit.prevent="signup">
-        <div class="text-red" v-if="error">{{ error }}</div>
+<div class="max-w-sm m-auto my-8">
+  <div class="border p-10 border-grey-light shadow rounded">
+    <h3 class="text-2xl mb-6 text-grey-darkest">Sign Up</h3>
+    <form @submit.prevent="signup">
+      <div class="text-red" v-if="error">{{ error }}</div>
 
-        <div class="mb-6">
-          <label for="email" class="label">E-mail Address</label>
-          <input type="email" v-model="email" class="input" id="email" placeholder="David@davidbolton.dev">
-        </div>
-        <div class="mb-6">
-          <label for="password" class="label">Password</label>
-          <input type="password" v-model="password" class="input" id="password" placeholder="Password">
-        </div>
-        <div class="mb-6">
-          <label for="password" class="label">Password Confirmation</label>
-          <input type="password" v-model="password_confirmation" class="input" id="password_confirmation" placeholder="Password">
-        </div>
-        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-blue-400 hover:bg-blue-700 block w-full py-4 text-white items-center justify-center">Sign Up</button>
+      <div class="mb-6">
+        <label for="email" class="label">E-mail Address</label>
+        <input type="email" v-model="email" class="input" id="email" placeholder="David@davidbolton.dev" />
+      </div>
+      <div class="mb-6">
+        <label for="password" class="label">Password</label>
+        <input type="password" v-model="password" class="input" id="password" placeholder="Password" />
+      </div>
+      <div class="mb-6">
+        <label for="password" class="label">Password Confirmation</label>
+        <input type="password" v-model="password_confirmation" class="input" id="password_confirmation" placeholder="Password" />
+      </div>
+      <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-blue-400 hover:bg-blue-700 block w-full py-4 text-white items-center justify-center">Sign Up</button>
 
-        <div class="my-4"><router-link to="/" class="link-grey">Sign In</router-link></div>
-      </form>
-    </div>
+      <div class="my-4">
+        <router-link to="/" class="link-grey">Sign In</router-link>
+      </div>
+    </form>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'Signup',
-  data () {
+  name: "Signup",
+  data() {
     return {
-      email: '',
-      password: '',
-      password_confirmation: '',
-      error: ''
-    }
+      email: "",
+      password: "",
+      password_confirmation: "",
+      error: "",
+    };
   },
-  created () {
-    this.checkSignedIn()
+  created() {
+    this.checkedSignedIn();
   },
-  updated () {
-    this.checkSignedIn()
+  updated() {
+    this.checkedSignedIn();
   },
   methods: {
-    signin () {
-      this.$http.plain.post('/signup', { email: this.email, password: this.password, password_confirmation: this.password_confirmation })
-        .then(response => this.signinSuccessful(response))
-        .catch(error => this.signinFailed(error))
+    signup() {
+      this.$http.plain
+        .post("/signup", {
+          email: this.email,
+          password: this.password,
+          password_confirmation: this.password_confirmation,
+        })
+        .then((response) => this.signupSuccessful(response))
+        .catch((error) => this.signupFailed(error));
     },
-    signinSuccessful (response) {
+    signupSuccessful(response) {
       if (!response.data.csrf) {
-        this.signinFailed(response)
-        return
+        this.signupFailed(response);
+        return;
       }
-      localStorage.csrf = response.data.csrf
-      localStorage.signedIn = true
-      this.error = ''
-      this.$router.replace('/records')
+      localStorage.csrf = response.data.csrf;
+      localStorage.signedIn = true;
+      this.error = "";
+      this.$router.replace("/records");
     },
-    signinFailed (error) {
-      this.error = (error.response && error.response.data && error.response.data.error) || 'Oops! Something went wrong.'
-      delete localStorage.csrf
-      delete localStorage.signedIn
+    signupFailed(error) {
+      this.error =
+        (error.response && error.response.data && error.response.data.error) ||
+        "Something went wrong";
+      delete localStorage.csrf;
+      delete localStorage.signedIn;
     },
-    checkSignedIn () {
+    checkedSignedIn() {
       if (localStorage.signedIn) {
-        this.$router.replace('/records')
+        this.$router.replace("/records");
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
--- recordstore-front/src/router/index.js
+++ recordstore-front/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import SignIn from '@/components/SignIn.vue'
 import Signup from '@/components/Signup.vue'
+import Records from '@/components/records/Records.vue'
+import Artists from '@/components/artists/Artist.vue'
 
 Vue.use(Router)
 
@@ -9,6 +11,16 @@ export default new Router({
   // Updates URL
   mode: 'history',
   routes: [
+    {
+      path: '/artist',
+      name: 'Artists',
+      component: Artists
+    },
+    {
+      path: '/records',
+      name: 'Records',
+      component: Records
+    },
     {
       path: '/',
       name: 'Signin',
