68b3060df68028a2c8cf4d27089de7d8361f935f
smdeox <smdeox@live.com>
2020-08-24 21:16:10 -0400

    Added signin logic

--- recordstore-front/src/router/index.js
+++ recordstore-front/src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import SignIn from '@/components/SignIn.vue'
+//.vue for single file component
 //import HelloWorld from '@/components/HelloWorld'
 
 
 Vue.use(Router)
 
 export default new Router({
+  // Updates URL
+  mode: 'history',
   routes: [
     {
-     
+     path: '/'
+     name: 'Signin',
+     component: Signin
     }
   ]
 })
